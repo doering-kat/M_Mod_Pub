6821329bb02aee3d4d4998b3297fedcd568ca079
katdoering <kdoering@umces.edu>
2019-01-11 11:02:45 -0500

    Initial commit, make folders for results

---
+++ Code/START_HERE_create_dir_folders.R
@@ -0,0 +1,10 @@
+# Header ------------------------------------------------------------------------
+# Create folders in the directory for storing results
+# Written 11 Jan 2019 by Kathryn Doering
+# 
+# Run before heading to the folder labeled 1. 
+# Specify and create folders ---------------------------------------------------
+
+#Derived data
+dir.create("./Derived_Data") # for models and any other derived data (including tables)
+dir.create("./Figs") # for any figures
