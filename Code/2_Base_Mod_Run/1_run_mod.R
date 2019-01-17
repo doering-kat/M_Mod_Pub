4efd1efcf2df8bbd54ad3741703c3224b8b75a3e
katdoering <kdoering@umces.edu>
2019-01-17 15:32:15 -0500

    Small changes to be compatible with M_Mod_1.stan and associated functions. Also small formatting edits.

--- Code/2_Base_Mod_Run/1_run_mod.R
+++ Code/2_Base_Mod_Run/1_run_mod.R
@@ -6,9 +6,8 @@
 # (NOTE: NOAA codes are statistical catch areas in the MD portion of Chesapeake Bay)
 # 
 # This version of the model includes corrections for boxes persisting for longer
-# than 1 year, boxes that disarticulate before the box survey (contained in Rq) and
-# for the difference in efficiency between boxes and live oysters (also contained
-# in Rq)
+# than 1 year, boxes that disarticulate before the box survey and
+# for the difference in efficiency between boxes and live oysters
 # 
 # Written 9 Jan 2019 by Kathryn Doering
 # 
@@ -25,7 +24,7 @@ write_files <- T # Make false if do not want to write any data to files.
 # other options
 options(stringsAsFactors = F)
 #custom R functions.
-source("./Code/Funs/mod_d_3_3_output_funs.R", echo = F)
+source("./Code/Funs/M_Mod_1_output_funs.R", echo = F)
 
 # Specify and Create output paths ----------------------------------------------
 
@@ -63,13 +62,13 @@ n_ts <- as.integer(2)
 
 # Select the complete timeseries of data to use. 
 all_dat_ts <- SelectData(
-    dat = dat, 
-    NOAA_vec = NOAA_list_init, 
-    yr_0 = yr_0, 
-    yr_last = yr_last, 
-    remove_0 = FALSE,
-    n_ts = n_ts #min number of bars with complete time series required. 
-)
+                         dat = dat, 
+                         NOAA_vec = NOAA_list_init, 
+                         yr_0 = yr_0, 
+                         yr_last = yr_last, 
+                         remove_0 = FALSE,
+                         n_ts = n_ts #min number of bars with complete time series required. 
+                         )
 
 # Make diagnostic plots (note: takes ~ 30 s to run)
 PlotData(all_dat_ts = all_dat_ts, file_path = fig_spec_path, file_name = "Data_Plots.pdf")
@@ -81,13 +80,13 @@ mod_dat <- output$mod_dat
 # Run model---------------------------------------------------------------------
 
 mod_1 <- stan(
-               file = './Code/model_d_3_3.stan',
+               file = "./Code/Stan_Mods/M_Mod_1.stan",
                iter = 4000, #Started with 4000 iterations based on info from previous runs
                data = mod_dat,
                seed = my_seed,
-              #init = inits,
-           #control = list(adapt_delta = 0.99), # Uncomment if model has divergent transitions.
-             chains = 3
+               #init = inits,
+               #control = list(adapt_delta = 0.99), # Uncomment if model has divergent transitions.
+               chains = 3
               )
 
 # Save Model results and data --------------------------------------------------
@@ -96,5 +95,5 @@ saveRDS(mod_1, paste0(der_dat_spec_path,"/model.rda"))
 saveRDS(mod_dat, paste0(der_dat_spec_path,"/model_dat.rda"))
 write.csv(output$bar_key, paste0(der_dat_spec_path,"/bar_reg_key.csv"), row.names = F)
 write.csv(output$raw_mod_dat, paste0(der_dat_spec_path,"/raw_dat.csv"),row.names = F)
-write.csv(output$NOAA_vec, file = paste0(der_dat_spec_path,"/NOAA_list.csv"), row.names = F )
+write.csv(output$NOAA_vec, file = paste0(der_dat_spec_path,"/NOAA_vec.csv"), row.names = F )
 
