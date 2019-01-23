8cf84c144dc100a5c8c6988a6deb155037856160
katdoering <kdoering@umces.edu>
2019-01-23 11:57:10 -0500

    Clean up, and converted all loops to functions to use with lapply.

--- Code/3_DFA_Base_Mod/1_run_DFA.R
+++ Code/3_DFA_Base_Mod/1_run_DFA.R
@@ -13,7 +13,7 @@
 # 
 # Written by Kathryn Doering on 17 Jan 2019
 # 
-# clear global env -------------------------------------------------------------
+# Clear global env -------------------------------------------------------------
 
 rm(list = ls())
 
@@ -26,21 +26,12 @@ options(stringsAsFactors = F) #change the default
 # Get data ---------------------------------------------------------------------
 #get the data (model output)
 mod_results_path <- "./Derived_Data/2_Base_Mod_Run"
-#all_dat <- read.csv(paste0("./models_and_derived_data/", folder_name, "/median_M_all.csv"))
-# #Load model and data associated with it:
-# bar_reg_key <- read.csv(paste0(mod_results_path, "/1_run_mod/bar_reg_key.csv"))
-# raw_mod_dat <- read.csv(paste0(mod_results_path, "/1_run_mod/raw_dat.csv"))
-
+# summary of the model results for M
 mod_sum <- read.csv(paste0(mod_results_path, "/3_plot_M/M_dat_mod_boxcount.csv"))
-# mod <- readRDS(file = paste0(mod_results_path, "/1_run_mod/model.rda"))
-# mod_dat <- readRDS(file = paste0(mod_results_path, "/1_run_mod/model_dat.rda"))
-
-# NOAA_vec <- read.csv(paste0(mod_results_path, "/1_run_mod/NOAA_list.csv"))
-# NOAA_vec <- NOAA_vec$x
-
+# NOAA codes and their associated broader regions.
 regions <-  read.csv("./Data/Doering_Thesis_Regions_3.csv")
 
-# name and create folders to store results -------------------------------------
+# Name and create folders to store results -------------------------------------
 
 # name folders
 der_dat_gen_path <- "./Derived_Data/3_DFA_Base_Mod"
@@ -53,13 +44,15 @@ dir.create(der_dat_spec_path)
 dir.create(fig_gen_path)
 dir.create(fig_spec_path)
 
-# Set years --------------------------------------------------------------------
+# Set years, number of trends to test in the models  ---------------------------
 
 # Years
 yr_0 <- 1990
 yr_1   <- yr_0+1
 yr_max <- 2017
 nyears <- yr_max - yr_0 #not including year 0
+
+n_trends <- 1:4 # number of trends to consider in models.
 
 # Get DFA input  ----------------------------------------------------------------
 #instantaneous rates of median estimate of M, then zscored.
@@ -97,17 +90,23 @@ N_ts <- dim(dat_wide)[1]
 # 
 # Plot the data (zscored and demeaned). separtely each on its own plot, but the
 # same device.
-Plot_Data_Sep <- function(dat_wide, yr_1){
+# Plot_Data_Sep function: formals are:
+# dat_mat: numeric data to plot in a matrix where rows are NOAACOdes and cols are years
+# yr_1, the first year of the model runs
+# NOAA_code: the NOAA code name, which should be a rowname of dat_mat.
+# returns: NA, but creates a plot
+Plot_Data_Sep <- function(dat_mat, yr_1){
   function(NOAA_code){
-    plot(dat_wide[NOAA_code, ], xlab = "", ylab = "M index (Z-transformed)", bty = "L",
+    plot(dat_mat[NOAA_code, ], xlab = "", ylab = "M index (Z-transformed)", bty = "L",
       xaxt = "n", pch = 16,
       type = "b")
-    axis(1, 0:dim(dat_wide)[2] + 1, yr_1 + 0:dim(dat_wide)[2])
+    axis(1, 0:dim(dat_mat)[2] + 1, yr_1 + 0:dim(dat_mat)[2])
     title(NOAA_code)
+    return()
   }
 }
 # Make a closure from Plot_Data_Sep function to use with lapply
-Plot_Data_Sep_Clos <- Plot_Data_Sep(dat_wide = dat_wide, yr_1 = yr_1)
+Plot_Data_Sep_Clos <- Plot_Data_Sep(dat_mat = dat_wide, yr_1 = yr_1)
 
 png(paste0(fig_spec_path,"/inst_ts_zscored.png"), res = 300, width = 10, height =60, units = "in")
 par(mfrow = c(N_ts, 1), mai = c(0.5, 0.7, 0.1, 0.1), omi = c(0,
@@ -119,11 +118,14 @@ dev.off()
 # Alpha is used to add transparency; darker portions are where lines overlay each other,
 # Where as single lines are shown in gray. 
 # must use png, bmp, or jpeg for trancparency to show up
+# 
+# Plot_All function: plot all the data in dat_mat on the same plot, overlapping.
 # formals are:
 # dat_mat: numeric data to plot in a matrix where rows are NOAACOdes and cols are years
 # file_name: a string with .png extension specifying the name and file path where
 #   the plot wil be saved.
 # ylab: text for the y axis of the plot (a character single value.)
+# returns: NA
 Plot_All <- function(dat_mat, file_name, ylab){
     NOAA <- row.names(dat_mat) # get 
     png(file_name, res = 300, width = 8, height = 4, units = "in")
@@ -135,70 +137,49 @@ Plot_All <- function(dat_mat, file_name, ylab){
     # plot the lines
     lapply(seq_along(NOAA[-1]), function(N) lines(dat_mat[N, ], col = alpha("black", 0.5), lwd = 2))
     dev.off()
+    return()
 }
 # Plot the z scored data
-Plot_All(NOAA= NOAA, dat_mat = dat_wide, file_name = paste0(fig_spec_path,"/inst_ts_zscored_together.png"), ylab = "M index (Z-transformed)")
+Plot_All(dat_mat = dat_wide, file_name = paste0(fig_spec_path,"/inst_ts_zscored_together.png"), ylab = "M index (Z-transformed)")
 # Plot the non-z-scored data.
-Plot_All(NOAA= NOAA, dat_mat = dat_wide_no_Z, file_name = paste0(fig_spec_path,"/inst_ts_not_zscored_together.png"), ylab = "M (instantaneous)")
-
-# plot all the code together.
-# Alpha is used to add transparency; darker portions are where lines overlay each other,
-# Where as single lines are shown in gray. 
-# must use png, bmp, or jpeg for trancparency to show up
-png(paste0(fig_spec_path,"/inst_ts_zscored_together.png"), res = 300, width = 8, height = 4, units = "in")
-#par(mai = c(0.5, 0.7, 0.1, 0.1), omi = c(0,0, 0, 0))
-par(xaxs="i", yaxs="i")
-plot(dat_wide[1, ], xlab = "Year", ylab = "M index (Z-transformed)", bty = "L",
-  xaxt = "n", ylim = c(min(dat_wide)-0.1, max(dat_wide)+0.1), col = alpha("black", 0.5),
-  type = "l", lwd = 2)
-axis(1, 1:nyears, yr_1:yr_max)
-# plot the lines
-lapply(seq_along(NOAA[-1]), function(N) lines(dat_wide[N, ], col = alpha("black", 0.5), lwd = 2))
-dev.off()
-
-#Also plot non-zscored data
-png(paste0(fig_spec_path,"/inst_ts_together.png"), res = 300, width = 8, height = 4, units = "in")
-#par(mai = c(0.5, 0.7, 0.1, 0.1), omi = c(0,0, 0, 0))
-par(xaxs="i", yaxs="i")
-plot(dat_wide_no_Z[1, ], xlab = "Year", ylab = "M index (Z-transformed)", bty = "L",
-  xaxt = "n", ylim = c(min(dat_wide_no_Z)-0.1, max(dat_wide_no_Z)+0.1), col = alpha("black", 0.5),
-  type = "l", lwd = 2)
-axis(1, 1:nyears, yr_1:yr_max)
-for (i in 2:length(NOAA)) {
-  lines(dat_wide_no_Z[i, ], col = alpha("black", 0.5), lwd = 2)
-}
-dev.off()
+Plot_All(dat_mat = dat_wide_no_Z, file_name = paste0(fig_spec_path,"/inst_ts_not_zscored_together.png"), ylab = "M (instantaneous)")
 
 # Estimate and save  DFA models ------------------------------------------------
 # Code to compare multiple models. use the MARSS package to estimate.
 
-# set new control params
-con_list <-  list(minit=200, maxit=50000, conv.test.slope.tol = 0.1) #specify the max iterations
-# set up forms of R matrices
-R <- "diagonal and equal" #only use a diagonal and equal covariance structure
-max_m <- 4 #the maximum number of trends (test trends from 1 to max_m)
-
-# fit lots of models & store results
-# NOTE: this will take a long time to run!
-mod_list <- list()
-
-  for(m in 1:max_m) {
-    dfa_model <-  list(m=m, R=R) #put the R and m into the data.
-    tmp_mod <-  MARSS(dat_wide, model=dfa_model, control=con_list,
-      form="dfa", z.score=FALSE)
-    #add model to the list
-    mod_list[[m]] <- tmp_mod
-    #assign(paste("mod", m,"diagonal_and_equal", sep="_"), tmp_mod) #assign a variable name to the model
+# fit models & store results
+# NOTE: this will take a long time to run
+# Run_DFA function: create and runs DFA models using MARSS.
+# formals are:
+# R, the covariance matrix (same as in MARSS package)
+# dat_mat, the numeric matrix of data where NOAA codes are rows and years are cols
+# m, the number of trends, which will vary by model.
+# returns: a dfa_model fitted to dat_mat and for the specified covariance matrix
+# and number of trends.
+Run_DFA <- function(R = "diagonal and equal", dat_mat){
+  function(m){
+    # set the control params (min and max iterations, and the test to determine convergence)
+    con_list <-  list(minit=200, maxit=50000, conv.test.slope.tol = 0.1)
+    dfa_model <-  list(m=m, R=R) #put the R and m into the list of dfa model options.
+    # run the model
+    mod_run <-  MARSS(dat_mat, model=dfa_model, control=con_list, form="dfa", 
+                       z.score=FALSE)
+    return(mod_run)
   }
-
-# add names to the list of models
-names(mod_list) <- paste0("trends_",m, "_diagonal_and_equal") # name wiht the number
-#of trends and covariance matrix.
+}
+#create a closure with the data set (use default for covariance matrix)
+# use the same data set for all models.
+Run_DFA_Clos <- Run_DFA(dat_mat = dat_wide)
+# estimate a model for each of the trends using the closure.
+mod_list <- lapply(n_trends, Run_DFA_Clos)
+# add names to the list of models - number of trends and the cov matrix used.
+names(mod_list) <- paste0(n_trends,"_trends_diagonal_and_equal") 
 #save the list of models
-saveRDS(mod_list, paste0(der_dat_spec_path, "/dfa_mods.rda"))
+saveRDS(mod_list, paste0(der_dat_spec_path, "/dfa_mod_list.rda"))
 
 
 # Save intermediate data files -------------------------------------------------
+
 # The median M values used as input, ordered in a dataframe
 write.csv(Med_M, paste0(der_dat_spec_path, "/med_M_inst_df_ordered.csv"), row.names = F)
 #save non-zscored med M inst values in a matrix.
