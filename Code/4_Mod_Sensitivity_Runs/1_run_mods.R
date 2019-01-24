cea429804c94c96d00c4c989043008c5e5a94dff
katdoering <kdoering@umces.edu>
2019-01-24 13:41:20 -0500

    Initial commit. Note that 2 and 3 do not function (3 is just a skeleton with no code, only comments), and 1 may have bugs.

---
+++ Code/4_Mod_Sensitivity_Runs/1_run_mods.R
@@ -0,0 +1,131 @@
+# Header -----------------------------------------------------------------------
+# Run the necessary sensitivity models.
+
+# Scenarios: 
+# Modify percent of boxes disarticulating before the survey
+# Try 10% and 30%.
+# 
+# Modify the efficiency ratio, use the 5th and 95th percentiles of the 
+# distribution used for the prior (calculated in an excel document);
+# These are 0.49 and 5.09, respectively. 
+# 
+# Written 24 Jan 2019 by KD
+# # Clear global env--------------------------------------------------------------
+rm(list = ls())
+
+# Load Packages and set options ------------------------------------------------
+# packages
+library(tidyverse)
+library(rstan)
+#R stan options
+rstan_options(auto_write = T)
+options(mc.cores = parallel::detectCores())
+my_seed <- 123
+set.seed(my_seed) #seed set in R
+write_files <- T # Make false if do not want to write any data to files.
+# other options
+options(stringsAsFactors = F)
+#custom R functions.
+source("./Code/Funs/M_Mod_1_output_funs.R", echo = F)
+
+# Specify and Create output paths ----------------------------------------------
+
+# general derived data for all base model run scripts:
+der_dat_gen_path <- "./Derived_Data/4_Mod_Sensitivity_Runs"
+# subfolder for this script:
+der_dat_spec_path <- paste0(der_dat_gen_path, "/1_run_mods")
+fig_gen_path <- "./Figs/4_Mod_Sensitivity_Runs"
+fig_spec_path <- paste0(fig_gen_path, "/1_run_mods")
+# make the folders
+dir.create(der_dat_gen_path)
+dir.create(der_dat_spec_path)
+dir.create(fig_gen_path)
+dir.create(fig_spec_path)
+
+# Load Data ---------------------------------------------------------------------
+
+# Fall dredge survey individual tows, all measured in half bushel cultch
+dat <-  read.csv("./Derived_Data/1_Clean_Data/1_Clean_Fall_Survey/fall_survey_half_bu.csv")
+# the base model run (to call the model code, so that it does not need to be 
+# recompiled)
+base_mod <- readRDS("./Derived_Data/2_Base_Mod_Run/1_run_mod/model.rda")
+
+# Specify input values ---------------------------------------------------------
+
+#CHANGE NOAA codes here- want all possible NOAA codes
+NOAA_list_init <- sort(unique(dat$NOAACode)) #make all the NOAA codes the initial
+#list. (some will be deleted if the don't have sufficient data.)
+
+# Stick with these years to run the model due to the data available
+yr_0 <- 1990
+yr_last <- 2017
+
+# min number of bars with complete time series a bar needs to be included in the model
+n_ts <- as.integer(2)
+
+# changes to model inputs (for sensitivity runs.)
+sens_runs_pars <- data.frame(R_eff = c(1.79, 1.79, 0.49, 5.09), 
+                             frac_d_prev = c(0.1, 0.3, 0.2, 0.2)
+                             )
+
+# Modify data ------------------------------------------------------------------
+# same for all model versions: 
+# Select the complete time series of data to use. 
+all_dat_ts <- SelectData(
+                         dat = dat, 
+                         NOAA_vec = NOAA_list_init, 
+                         yr_0 = yr_0, 
+                         yr_last = yr_last, 
+                         remove_0 = FALSE,
+                         n_ts = n_ts #min number of bars with complete time series required. 
+                        )
+
+# Make diagnostic plots (note: takes ~ 30 s to run)
+# Should be the same as for base run, so no need.
+# PlotData(all_dat_ts = all_dat_ts, file_path = fig_spec_path, file_name = "Data_Plots.pdf")
+
+#Get data sets to run the model as well as use for plotting later
+# differs among mod runs. 
+
+# loop through the sensitivity runs.
+# The only changes will be for R_eff and frac_d_prev.
+
+for (i in 1:nrow(sens_runs_pars)) {
+  # get the R_eff value and frac value for the urn
+  tmp_R_eff <- sens_runs_pars[i, "R_eff"]
+  tmp_frac_d_prev <- sens_runs_pars[i, "frac_d_prev"]
+  # make the model data
+  output <- CreateModDataList(all_dat_ts = all_dat_ts, R_eff = tmp_R_eff, frac_d_prev = tmp_frac_d_prev)
+  mod_dat <- output$mod_dat
+  # Run model-------------------------------------------------------------------
+  # WARNING: Run may take several hours with the whole fall survey database.
+  mod <- stan(
+          #file = "./Code/Stan_Mods/M_Mod_1.stan",
+          # use same model as the base_mod to avoid recompiling; can use the file
+          #  argument instead of the fit one to recompile model and run. 
+          fit = base_mod,
+          iter = 4000, #Started with 4000 iterations based on info from previous runs
+          data = mod_dat,
+          seed = my_seed,
+          #init = inits, # uncomment if want to provide initial values for the model; otherwise, used defaults
+          #control = list(adapt_delta = 0.99), # Uncomment if model has divergent transitions.
+          chains = 3
+  )
+  
+  # Save Model results and data ------------------------------------------------
+  
+  saveRDS(mod, paste0(der_dat_spec_path,"/R_eff_", round(tmp_R_eff, 2), "_frac_d_prev_", 
+    round(tmp_frac_d_prev, 1), "_model.rda"))
+  saveRDS(mod_dat, paste0(der_dat_spec_path, "/R_eff_", round(tmp_R_eff, 2), "_frac_d_prev_", 
+    round(tmp_frac_d_prev, 1), "_model_dat.rda"))
+  
+  # delete model (to reduce obj in workspace) ----------------------------------
+  rm(mod)
+}
+
+# Save files that do not differ across models ----------------------------------
+# all below should be the same as base run and all runs, but just save once (just in case).
+write.csv(sens_runs_pars, paste0(der_dat_spec_path, "/sense_runs_pars.csv"))
+write.csv(output$bar_key, paste0(der_dat_spec_path,"/bar_reg_key.csv"), row.names = F)
+write.csv(output$raw_mod_dat, paste0(der_dat_spec_path,"/raw_dat.csv"),row.names = F)
+write.csv(output$NOAA_vec, file = paste0(der_dat_spec_path,"/NOAA_vec.csv"), row.names = F )
---
+++ Code/4_Mod_Sensitivity_Runs/2_check_convergence.R
@@ -0,0 +1,84 @@
+# Header -----------------------------------------------------------------------
+# 
+# Check model convergence and make diagnostic plots
+# Use Rhat, and divergent transitions to determine if there are any indications
+# that the model did not converge.
+# 
+# Written 11 Jan 2019 by Kathryn Doering
+# 
+# Clear global env--------------------------------------------------------------
+rm(list = ls())
+
+# Load Packages and set options ------------------------------------------------
+library(tidyverse)
+library(rstan)
+options(stringsAsFactors = F)
+
+# Load Data --------------------------------------------------------------------
+#Load model and data associated with it: for all runs.
+run_mod_path <- "./Derived_Data/4_Mod_Sensitivity_Runs/1_run_mod"
+# bar_reg_key <- read.csv(paste0(run_mod_path, "/bar_reg_key.csv"))
+# raw_mod_dat <- read.csv(paste0("./models_and_derived_data/", folder_name, "/raw_dat.csv"))
+
+#NoAA_vec is in the order the model was run
+NOAA_vec <- read.csv(paste0(run_mod_path,"/NOAA_vec.csv"))
+NOAA_vec <- NOAA_vec$x
+
+sens_runs_pars <- read.csv(paste0(run_mod_path, "/sense_runs_pars.csv"))
+
+# Set output paths and make folders --------------------------------------------
+
+# general derived data for all base model run scripts:
+der_dat_gen_path <- "./Derived_Data/2_Base_Mod_Run"
+# subfolder for this script:
+der_dat_spec_path <- paste0(der_dat_gen_path, "/2_check_convergence")
+# fig_gen_path <- "./Figs/2_Base_Mod_Run"
+# fig_spec_path <- paste0(fig_gen_path, "/2_check_convergence")
+# make the folders (gen folders should already exist)
+dir.create(der_dat_spec_path)
+# dir.create(fig_spec_path) 
+
+# loop through models to check convergence.-------------------------------------
+file_names <- paste0(run_mod_path,"/R_eff_", round(sens_runs_pars$R_eff, 2), "_frac_d_prev_", 
+                round(sens_runs_pars$frac_d_prev, 1))
+
+# Load models (note: model files are really big, so may need to load one at a time instead!)
+mods <- purrr::map(file_names, ~readRDS(paste0(.x, "_model.rda"))) 
+mod_dat <- purrr::map(file_names, ~readRDS(file = paste0(.x, "_model_dat.rda")))
+
+# Elapsed time -----------------------------------------------------------------
+# #see how long sampling took
+purrr::map(mods, ~print(get_elapsed_time(.x)))
+
+# Check for divergent samples --------------------------------------------------
+# See if any divergent samples
+for(i in 1:length(mods)){
+  tmp_mod <- mods[[i]]
+  sampler_params <- get_sampler_params(tmp_mod, inc_warmup = FALSE)
+  div_samples_by_chain <- sapply(sampler_params, function(x) sum(x[, "divergent__"]))
+  print(div_samples_by_chain) # if all 0, then there were no divergent samples.
+  if(sum(div_samples_by_chain)>0) warning(paste0("There are divergent samples in model ", i ))
+}
+
+# Check for sufficient r hat and effective sample size -------------------------
+# Want all parameters to have effective sample sizes (neff) >1000
+# TODO: Edit below here to run for all models.
+# get param summaries, includes neff and rhat
+all_par_summary <-purrr::map(mods, ~summary(.x)$summary)
+
+# Rhat
+# put all rhat values in a vector and check
+all_par_rhat <- purrr::map(all_par_summary,[,"Rhat"])
+# warn if there are r hat values above 1.1
+max(all_par_rhat)
+if(max(all_par_rhat)>1.1) warning("There are Rhat values > 1.1")
+
+# neff
+# put all n_effective values in a vector and check
+all_par_neff <- all_par_summary[,"n_eff"]
+# see if neff is adequate.
+min(all_par_neff) # Hopefully, all above 1,000. (or close to 1,000)
+if(any(all_par_neff <1000)) warning("There were parameters with neff < 1000")
+
+# write summary to a file ------------------------------------------------------
+write.csv(all_par_summary, paste0(der_dat_spec_path, "/mod_summary_all_chains.csv"),row.names = T)
