1ec60e275448b34d0a03d27166089cb1d5e4b06c
doering-kat <doering.kat@gmail.com>
2019-08-31 11:24:55 -0700

    add roxygen documentation, plot unmodeled NOAA codes as black instead of white
    Note that for some reason, could not add the black NOAA code layer within the function only outside of...not sure exactly why, something with environments?

--- Code/2_Base_Mod_Run/8_plot_M_map_lat_long.R
+++ Code/2_Base_Mod_Run/8_plot_M_map_lat_long.R
@@ -7,6 +7,7 @@ library(tidyverse)
 library(rstan)
 library(sp)
 library(RColorBrewer)
+library(latticeExtra)
 options(stringsAsFactors = F) #Change options.
 
 # Load data ---------------------------------------------------------------------
@@ -47,27 +48,37 @@ NOAA_latlon <- sp::spTransform(NOAA, sp::CRS('+proj=longlat'))
 NOAAlab <- sp::spTransform(NOAAlab, sp::CRS('+proj=longlat'))
 
 
-# Plotting function
-# Plot_M_Map_latlon: a function to plot natural mortality values on a spatial object for
-# a given year. Function inputs are:
-# M, a dataframe with columns Year, M_annual, and NOAACODE
-# Yr, a single integer value with the year of M_annual to plot.
-# NOAA, an sp spatial polygon data.frame with the NOAACODE polygons.
-# lab, a single logical value. True if NOAA code labels are desired on the plot,
-#   or False if no labels are wanteed.
-# NOAAlab, an sp spatial points data.frame including the labels and where they should be placed.
-# axes: a signle logical value, true if want axes in northing and easting, false if not.
-# axes_lab: a single logical value, true if want x and y axis labels, false if not.
-# title: a single logical value, true if want the year as the title on the plot.
-# filepath: specify where the plots should be made. Does not include the filename.
-# filename: an optional name for  the file. should have extension ".pdf"
-# palette: a character vector of color names. length should be 1 less than palette breaks.
-# palette_breaks: a numeric vector specifying breaks in the scalebar. This can be 
-# specified or left as optional. If not specified, function will calculate by scaling
-# equally using the number of categories in the palette between 0 and 1.
-# returns: an sp plot object (also creates and saves plot in a graphic device.)
+#' Plotting function
+#' Plot_M_Map_latlon: a function to plot natural mortality values on a spatial object for
+#' a given year. Function inputs are:
+#' @param M a dataframe with columns Year, M_annual, and NOAACODE
+#' @param Yr a single integer value with the year of M_annual to plot.
+#' @param NOAA an sp spatial polygon data.frame with the NOAACODE polygons.
+#'  lab, a single logical value. True if NOAA code labels are desired on the plot,
+#'  or False if no labels are wanteed.
+#' @param NOAAlab an sp spatial points data.frame including the labels and 
+#'  where they should be placed.
+#' @param axes a signle logical value, true if want axes in northing and 
+#'  easting, false if not.
+#' @param axes_lab a single logical value, true if want x and y axis labels, 
+#'  false if not.
+#' @param title a single logical value, true if want the year as the title on 
+#'  the plot.
+#' @param title_text optional text to add to the title if title = FALSE.
+#' @param write_file should the figure be written to a file? defaults to FALSE.
+#' @param filepath specify where the plots should be made. Does not include the
+#'  filename.
+#' @param filename an optional name for  the file. should have extension ".pdf"
+#' @param palette a character vector of color names. length should be 1 less
+#'  than palette breaks.
+#' @param palette_breaks a numeric vector specifying breaks in the scalebar. 
+#'  This can be specified or left as optional. If not specified, function will
+#'  calculate by scaling equally using the number of categories in the palette 
+#'  between 0 and 1.
+#' @return an sp plot object invisibly. Side effect is creates and saves plot 
+#'  in a graphic device.)
 Plot_M_Map_latlon <- function(M, colname = "M_Annual" , NOAA, lab = F, NOAAlab = NULL, 
-  axes = F, axes_lab = F, title = T, title_text = "Year here", 
+  axes = F, axes_lab = F, title = T, title_text = "Year here", write_file = FALSE,
   filepath = ".", filename = NA, 
   palette = rev(RColorBrewer::brewer.pal(n = 10, name = "RdYlBu")), 
   palette_breaks = NULL) {
@@ -102,17 +113,13 @@ Plot_M_Map_latlon <- function(M, colname = "M_Annual" , NOAA, lab = F, NOAAlab =
     scales_list <- list(draw = F)
     par_settings_list <- list(fontsize = list(text = 20), axis.line = list(col = "transparent"))
   }
-  # open the graphics device.
-  if(is.na(filename)){
-    pdf(paste0(filepath, "/M_map_latlon.pdf"), width = 9, height = 11)
-  } else{
-    pdf(paste0(filepath,"/", filename), width = 9, height = 11)
-  }
-  # create the pplot
+
   plot <- sp::spplot(NOAA, #spatial object to plot
            zcol =  colname, #column name in the dataframe to fill.
            # year in title, if desired
-           main = ifelse(title == T, list(label = title_text, cex = 1.2), list(NULL)),
+           main = ifelse(title == T, 
+                         list(label = title_text, cex = 1.2), 
+                         list(NULL)),
            scales = scales_list,
            # plot limits:
            ylim = c(NOAA@bbox[2,1] - .1, NOAA@bbox[2,2] + .1),
@@ -126,10 +133,16 @@ Plot_M_Map_latlon <- function(M, colname = "M_Annual" , NOAA, lab = F, NOAAlab =
            sp.layout = ifelse(lab == T, list(sl_labels), list(NULL)),
            par.settings = par_settings_list
            )
-
-  print(plot) # to make visible in device
-  dev.off()
-  return(plot)
+  if(write_file == TRUE){
+    if(is.na(filename)){
+      pdf(paste0(filepath, "/M_map_latlon.pdf"), width = 9, height = 11)
+    } else{
+      pdf(paste0(filepath,"/", filename), width = 9, height = 11)
+    }
+    print(plot)
+    dev.off()
+  }
+  invisible(plot)
 }
 
 # Make the by year plots -------------------------------------------------------
@@ -139,17 +152,22 @@ cnt <- 1
 for (y in 1991:2017) {
   tmp_M <- dplyr::filter(M_map_df, Year == y) # just M for the year.
   # define the color brewer palette.name))
-maps[[cnt]] <-   Plot_M_Map_latlon(M = tmp_M, NOAA = NOAA_latlon, colname = "M_annual", 
-    filepath = fig_spec_path,axes = T,axes_lab = T, filename = paste0("M_", y, ".pdf"), 
-    title_text = as.character(y))
-cnt <- cnt + 1
-# to change the color of the unmodeled plots, will need to use this and resave the 
-#plot.
-#saved_plot <- saved_plot + latticeExtra::layer_(sp.polygons(NOAA_latlon,fill='gray70')) # fill in other NOAA codes with gray..
-# Now, export saved_plot as a pdf to save with other fill.
+  maps[[cnt]] <-   Plot_M_Map_latlon(M = tmp_M, 
+                                     NOAA = NOAA_latlon, 
+                                     colname = "M_annual", 
+                                     filepath = fig_spec_path,
+                                     axes = T,
+                                     axes_lab = T,
+                                     filename = paste0("M_", y, ".pdf"), 
+                                     title_text = as.character(y))
+  
+  pdf(file.path(fig_spec_path, paste0("M_", y, ".pdf")), width = 9, height = 11)
+  print(maps[[cnt]] + 
+    latticeExtra::layer_(sp.polygons(NOAA_latlon,fill='black')))
+  dev.off()
+  cnt <- cnt + 1
 }
-# TODO: use maps object to make plots all together.
-# 
+
 # Make the avg and sd plots ----------------------------------------------------
 # Calculate mean and sd of median values by NOAA code
 Avg_SD_M_df <- M_map_df %>% 
@@ -161,7 +179,7 @@ max_plot_avg <- ceiling(10*max(Avg_SD_M_df$Mean))/10 # the maximum value to plot
 n_plot_avg <- as.integer(max_plot_avg*10*2)
 avg_pal <- RColorBrewer::brewer.pal(n = n_plot_avg+1, name = "Greys")[-1] #get rid of the white
 avg_pal_breaks <- seq(from = 0, to = max_plot_avg, length.out = length(avg_pal)+1)
-Plot_M_Map_latlon(M = Avg_SD_M_df, NOAA = NOAA_latlon, colname = "Mean", filepath = fig_spec_path, axes = T,axes_lab = T,
+Plot_M_Map_latlon(M = Avg_SD_M_df, NOAA = NOAA_latlon, colname = "Mean", filepath = fig_spec_path, axes = T,axes_lab = T, write_file = TRUE,
   filename = "M_Med_Mean_Map.pdf", title_text = "A. Mean", palette = avg_pal, palette_breaks = avg_pal_breaks )
 
 # plot SD
@@ -169,7 +187,7 @@ max_plot_SD <- ceiling(10*max(Avg_SD_M_df$SD))/10 # the maximum value to plot
 n_plot_SD <- as.integer(max_plot_SD*10*2)
 SD_pal <- RColorBrewer::brewer.pal(n = n_plot_SD+1, name = "Greys")[-1] #get rid of the white
 SD_pal_breaks <- seq(from = 0, to = max_plot_SD, length.out = length(SD_pal)+1)
-Plot_M_Map_latlon(M = Avg_SD_M_df, NOAA = NOAA_latlon, colname = "SD", filepath = fig_spec_path, axes = T,axes_lab = T,
+Plot_M_Map_latlon(M = Avg_SD_M_df, NOAA = NOAA_latlon, colname = "SD", filepath = fig_spec_path, axes = T,axes_lab = T, write_file = TRUE, 
   filename = "M_Med_SD_Map.pdf", title_text = "B. SD", palette = SD_pal, palette_breaks = SD_pal_breaks )
 
 
